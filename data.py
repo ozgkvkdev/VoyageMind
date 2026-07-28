c361c38bd96dc9fdb6980980673fb18cc2109c1f
özgür kavak <ozgurkvk15@gmail.com>
2026-07-28 22:29:09 +0300

    feat: seyahat şehir verileri eklendi

---
+++ data.py
@@ -0,0 +1,39 @@
+# şehir , günlük ücret , aktiviteler bilgileri.
+
+antalya = {
+    "sehir": "Antalya",
+    "gunluk_ucret": 3200,
+    "aktiviteler": ["Deniz", "Kamp", "Doğa", "Tekne Turu", "Yüzme"]
+}
+
+kapadokya = {
+    "sehir": "Kapadokya",
+    "gunluk_ucret": 2800,
+    "aktiviteler": ["Balon", "Fotoğrafçılık", "Doğa", "Tarih", "Kamp"]
+}
+
+istanbul = {
+    "sehir": "İstanbul",
+    "gunluk_ucret": 3500,
+    "aktiviteler": ["Müze", "Alışveriş", "Yemek", "Boğaz Turu", "Tarih"]
+}
+
+trabzon = {
+    "sehir": "Trabzon",
+    "gunluk_ucret": 2300,
+    "aktiviteler": ["Yayla", "Doğa", "Kamp", "Yürüyüş", "Fotoğrafçılık"]
+}
+
+izmir = {
+    "sehir": "İzmir",
+    "gunluk_ucret": 2700,
+    "aktiviteler": ["Deniz", "Bisiklet", "Yemek", "Tarih", "Alışveriş"]
+}
+
+sehirler = [
+    antalya,
+    kapadokya,
+    istanbul,
+    trabzon,
+    izmir
+]
